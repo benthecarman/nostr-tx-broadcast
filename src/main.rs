97ee4b6d11c0150b944d447536a012ab751d6c37
benthecarman <benthecarman@live.com>
2023-05-05 09:31:00 -0500

    Skip empty txs

--- src/main.rs
+++ src/main.rs
@@ -86,6 +86,10 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn broadcast_txs(txs: Vec<Transaction>, magic: Magic) -> anyhow::Result<()> {
+    if txs.is_empty() {
+        return Ok(());
+    }
+
     let client = reqwest::Client::builder().build()?;
 
     let mutinynet = Magic::from_bytes([0xA5, 0xDF, 0x2D, 0xCB]);
